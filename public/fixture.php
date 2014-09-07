32c23c014e504811c7af6b1092ac300ff7d33c23
crucifyextreme <gmferreir@hotmail.com>
2014-09-07 19:39:04 -0300

    Desenvolvendo Projeto

---
+++ public/fixture.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guilhermeferreira
+ * Date: 07/09/14
+ * Time: 10:22
+ */ 
---
+++ src/SON/Pdo/Configuracao.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guilhermeferreira
+ * Date: 06/09/14
+ * Time: 21:00
+ */
+
+namespace SON\Pdo;
+
+
+class Configuracao {
+
+} 
